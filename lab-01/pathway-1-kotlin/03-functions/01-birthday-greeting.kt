62c764b977768626d039b2fb5103f2cd4df9484f
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    feat(lab-01): add name parameter to birthdayGreeting

--- lab-01/pathway-1-kotlin/03-functions/01-birthday-greeting.kt
+++ lab-01/pathway-1-kotlin/03-functions/01-birthday-greeting.kt
@@ -2,7 +2,7 @@ fun main() {
     println(birthdayGreeting())
 }
 
-fun birthdayGreeting(): String {
+fun birthdayGreeting(name: String): String {
     val nameGreeting = "Happy Birthday, Rover!"
     val ageGreeting = "You are now 5 years old!"
     return "$nameGreeting\n$ageGreeting"
