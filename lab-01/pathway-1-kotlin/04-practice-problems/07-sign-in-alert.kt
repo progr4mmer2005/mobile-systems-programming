a8afdfa30c2d7bfb75429d077bbfc71bdb8d9769
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    fix(lab-01): practice 8 - default the operating system to Unknown OS

--- lab-01/pathway-1-kotlin/04-practice-problems/07-sign-in-alert.kt
+++ lab-01/pathway-1-kotlin/04-practice-problems/07-sign-in-alert.kt
@@ -1,11 +1,24 @@
 fun main() {
-    val operatingSystem = "Chrome OS"
-    val emailId = "sample@gmail.com"
+    val firstUserEmailId = "user_one@gmail.com"
 
-    println(displayAlertMessage(operatingSystem, emailId))
+    // The following line of code assumes that you named your parameter as emailId.
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
 }
 
-// Define your displayAlertMessage() below this line.
-fun displayAlertMessage(operatingSystem: String, emailId: String): String {
+fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailId: String): String {
     return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
 }
