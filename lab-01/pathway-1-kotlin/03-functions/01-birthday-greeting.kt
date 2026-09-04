38f0679177b8b628790f14aed08dbf4d5344bac8
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    feat(lab-01): return both greetings joined by a newline escape

--- lab-01/pathway-1-kotlin/03-functions/01-birthday-greeting.kt
+++ lab-01/pathway-1-kotlin/03-functions/01-birthday-greeting.kt
@@ -5,4 +5,5 @@ fun main() {
 fun birthdayGreeting(): String {
     val nameGreeting = "Happy Birthday, Rover!"
     val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
 }
