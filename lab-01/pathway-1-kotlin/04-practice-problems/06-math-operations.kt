c0854ccaf918e8d7596b365a54d8d3bfe21eeaf6
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    feat(lab-01): practice 7 - call an add function that is not defined yet

--- lab-01/pathway-1-kotlin/04-practice-problems/06-math-operations.kt
+++ lab-01/pathway-1-kotlin/04-practice-problems/06-math-operations.kt
@@ -1,7 +1,13 @@
 fun main() {
     val firstNumber = 10
     val secondNumber = 5
-    val result = firstNumber + secondNumber
+    val thirdNumber = 8
+
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = add(firstNumber, thirdNumber)
 
     println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
 }
+
+// Define add() function below this line
