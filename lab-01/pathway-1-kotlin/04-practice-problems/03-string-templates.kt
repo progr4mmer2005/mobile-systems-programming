06d81c66a3ed940c963910372f673cf5e791d871
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    fix(lab-01): practice 4 - declare reassigned variables with var

--- lab-01/pathway-1-kotlin/04-practice-problems/03-string-templates.kt
+++ lab-01/pathway-1-kotlin/04-practice-problems/03-string-templates.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val discountPercentage: Int = 0
-    val offer: String = ""
+    var discountPercentage: Int = 0
+    var offer: String = ""
     val item = "Google Chromecast"
     discountPercentage = 20
     offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
