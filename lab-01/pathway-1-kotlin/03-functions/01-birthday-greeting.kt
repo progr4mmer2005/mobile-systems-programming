e9909b6685b947df848870de5331e891d2a4ba13
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    feat(lab-01): omit the name argument in both calls

--- lab-01/pathway-1-kotlin/03-functions/01-birthday-greeting.kt
+++ lab-01/pathway-1-kotlin/03-functions/01-birthday-greeting.kt
@@ -1,6 +1,6 @@
 fun main() {
     println(birthdayGreeting(age = 5))
-    println(birthdayGreeting("Rex", 2))
+    println(birthdayGreeting(age = 2))
 }
 
 fun birthdayGreeting(name: String = "Rover", age: Int): String {
