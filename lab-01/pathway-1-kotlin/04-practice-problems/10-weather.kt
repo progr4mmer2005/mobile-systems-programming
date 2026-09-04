b39c6df6882dc0dac22f3edd0c92bcc84a164bca
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    fix(lab-01): practice 11 - turn the variables into function parameters

--- lab-01/pathway-1-kotlin/04-practice-problems/10-weather.kt
+++ lab-01/pathway-1-kotlin/04-practice-problems/10-weather.kt
@@ -1,8 +1,8 @@
 fun main() {
-    printWeatherForCity()
+    printWeatherForCity("Ankara", 27, 31, 82)
 }
 
-fun printWeatherForCity() {
+fun printWeatherForCity(cityName: String, lowTemp: Int, highTemp: Int, chanceOfRain: Int) {
     println("City: $cityName")
     println("Low temperature: $lowTemp, High temperature: $highTemp")
     println("Chance of rain: $chanceOfRain%")
