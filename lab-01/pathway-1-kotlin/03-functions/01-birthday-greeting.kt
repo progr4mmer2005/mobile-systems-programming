5f5d0b650340cf8e61d4ae3137203789fe8c0836
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    feat(lab-01): give the name parameter a default value

--- lab-01/pathway-1-kotlin/03-functions/01-birthday-greeting.kt
+++ lab-01/pathway-1-kotlin/03-functions/01-birthday-greeting.kt
@@ -3,8 +3,6 @@ fun main() {
     println(birthdayGreeting(age = 2, name = "Rex"))
 }
 
-fun birthdayGreeting(name: String, age: Int): String {
-    val nameGreeting = "Happy Birthday, $name!"
-    val ageGreeting = "You are now $age years old!"
-    return "$nameGreeting\n$ageGreeting"
+fun birthdayGreeting(name: String = "Rover", age: Int): String {
+    return "Happy Birthday, $name! You are now $age years old!"
 }
