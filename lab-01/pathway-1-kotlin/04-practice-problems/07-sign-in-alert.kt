2131e488a919477e7ab4c572e44de7ec62419acf
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    feat(lab-01): practice 8 - build a sign-in alert message from two parameters

---
+++ lab-01/pathway-1-kotlin/04-practice-problems/07-sign-in-alert.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val operatingSystem = "Chrome OS"
+    val emailId = "sample@gmail.com"
+
+    println(displayAlertMessage(operatingSystem, emailId))
+}
+
+// Define your displayAlertMessage() below this line.
+fun displayAlertMessage(operatingSystem: String, emailId: String): String {
+    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+}
