079e718faa579d6dc3881465cfcb87526592ee8f
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-04 12:00:00 +0000

    refactor(lab-01): practice 9 - rename to camelCase and drop the stray semicolon

--- lab-01/pathway-1-kotlin/04-practice-problems/08-pedometer.kt
+++ lab-01/pathway-1-kotlin/04-practice-problems/08-pedometer.kt
@@ -1,11 +1,11 @@
 fun main() {
-    val Steps = 4000
-    val caloriesBurned = PEDOMETERstepsTOcalories(Steps);
-    println("Walking $Steps steps burns $caloriesBurned calories") 
+    val steps = 4000
+    val caloriesBurned = pedometerStepsToCalories(steps)
+    println("Walking $steps steps burns $caloriesBurned calories")
 }
 
-fun PEDOMETERstepsTOcalories(NumberOFStepS: Int): Double {
-    val CaloriesBURNEDforEachStep = 0.04
-    val TotalCALORIESburned = NumberOFStepS * CaloriesBURNEDforEachStep
-    return TotalCALORIESburned
+fun pedometerStepsToCalories(numberOfSteps: Int): Double {
+    val caloriesBurnedForEachStep = 0.04
+    val totalCaloriesBurned = numberOfSteps * caloriesBurnedForEachStep
+    return totalCaloriesBurned
 }
