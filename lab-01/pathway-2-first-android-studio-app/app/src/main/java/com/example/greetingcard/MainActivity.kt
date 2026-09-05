734c8205590b9fcf10c35067bc6516b71036a34d
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-05 18:58:15 +0300

    feat(lab-01): change showBackground param back to true

--- lab-01/pathway-2-first-android-studio-app/app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ lab-01/pathway-2-first-android-studio-app/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -38,7 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
