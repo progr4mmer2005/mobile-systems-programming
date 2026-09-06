ee9f59d863562dd76d3e79135f43d1973feb49d7
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 14:42:48 +0300

    feat(lab-01): declare a GreetingImage composable

--- lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -61,6 +61,10 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String) {
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
