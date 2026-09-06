761dc88a49bb289104212c0e4ffab51a85474672
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:56:43 +0300

    feat(classes-and-objects): reassign the variable to a light and call turnOn again

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
@@ -122,4 +122,7 @@ class SmartHome(
 fun main() {
     var smartDevice: SmartDevice = SmartTvDevice("Android TV", "Entertainment")
     smartDevice.turnOn()
+
+    smartDevice = SmartLightDevice("Google Light", "Utility")
+    smartDevice.turnOn()
 }
