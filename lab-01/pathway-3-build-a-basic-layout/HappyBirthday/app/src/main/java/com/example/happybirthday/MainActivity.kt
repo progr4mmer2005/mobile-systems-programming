d3033c40a0d3ec2209be37845ee1c64f77f34414
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 13:13:08 +0300

    feat(lab-01): set the signature font size to 36.sp

--- lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -35,7 +35,8 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
         lineHeight = 116.sp,
     )
     Text(
-        text = from
+        text = from,
+        fontSize = 36.sp
     )
 }
 
