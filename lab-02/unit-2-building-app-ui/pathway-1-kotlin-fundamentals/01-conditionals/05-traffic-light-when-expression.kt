006f8cd6d982ce2d7b245116c4516271cb9f07c3
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:07:53 +0300

    feat(conditionals): turn the when statement into an expression

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/05-traffic-light-when-expression.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/05-traffic-light-when-expression.kt
@@ -1,10 +1,10 @@
 fun main() {
     val trafficLightColor = "Amber"
 
-    when (trafficLightColor) {
-        "Red" -> println("Stop")
-        "Yellow", "Amber" -> println("Slow")
-        "Green" -> println("Go")
-        else -> println("Invalid traffic-light color")
+    val message = when (trafficLightColor) {
+        "Red" -> "Stop"
+        "Yellow", "Amber" -> "Slow"
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
     }
 }
