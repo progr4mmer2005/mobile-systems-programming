a252cfe5c219ff373a2d5b4fddc2243bcff213c7
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:24:06 +0300

    feat(nullability): empty both branches before filling them with values

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/02-nullability/06-null-check-expression.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/02-nullability/06-null-check-expression.kt
@@ -2,8 +2,6 @@ fun main() {
     var favoriteActor: String? = "Sandra Oh"
 
     val lengthOfName = if (favoriteActor != null) {
-        println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
     } else {
-        println("You didn't input a name.")
     }
 }
