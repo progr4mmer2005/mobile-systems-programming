ddab0bc5b3c791c8d779a10ec83e9a4b0f5ed505
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:23:10 +0300

    feat(nullability): report the missing name in the else branch

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/02-nullability/05-null-check-if.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/02-nullability/05-null-check-if.kt
@@ -4,5 +4,6 @@ fun main() {
     if (favoriteActor != null) {
         println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
     } else {
+        println("You didn't input a name.")
     }
 }
