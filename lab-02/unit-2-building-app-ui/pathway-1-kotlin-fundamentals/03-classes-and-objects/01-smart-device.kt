a127bbed1c2f5981f34e56357c1e000092d462ef
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:48:21 +0300

    fix(classes-and-objects): pass the constructor arguments when creating the object

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
@@ -12,7 +12,7 @@ class SmartDevice(val name: String, val category: String) {
 }
 
 fun main() {
-    val smartTvDevice = SmartDevice()
+    val smartTvDevice = SmartDevice("Android TV", "Entertainment")
     println("Device name is: ${smartTvDevice.name}")
     smartTvDevice.turnOn()
     smartTvDevice.turnOff()
