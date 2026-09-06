b33e3fc13660d78c05ee05165c06242762c99ae4
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 18:15:10 +0300

    feat(function-types): pass the lambda straight into the call

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/04-function-types-and-lambdas/01-trick-function.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/04-function-types-and-lambdas/01-trick-function.kt
@@ -3,7 +3,7 @@ fun main() {
         "$it quarters"
     }
 
-    val treatFunction = trickOrTreat(false, coins)
+    val treatFunction = trickOrTreat(false, { "$it quarters" })
     val trickFunction = trickOrTreat(true, null)
     treatFunction()
     trickFunction()
