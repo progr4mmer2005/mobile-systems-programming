f6ad9ff5d6857fadd328a7d06872ee529585bc84
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:51:42 +0300

    feat(classes-and-objects): add a speaker volume property with a range check

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
@@ -13,6 +13,13 @@ open class SmartDevice(val name: String, val category: String) {
 
 class SmartTvDevice(deviceName: String, deviceCategory: String) :
     SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
 }
 
 fun main() {
