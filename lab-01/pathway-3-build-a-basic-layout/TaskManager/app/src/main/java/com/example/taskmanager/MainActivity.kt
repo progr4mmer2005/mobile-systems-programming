9d0725d3c5de4b017b7e298bfb39911d16997d2e
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 15:08:34 +0300

    feat(task-manager): center the completion icon on the screen

--- lab-01/pathway-3-build-a-basic-layout/TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TaskCompletedScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskCompletedScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TaskCompletedScreenPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        TaskCompletedScreen()
     }
 }
