55dd9cc3147b9a8e2c8cf48905c52fa41697c160
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:52:23 +0300

    feat(classes-and-objects): add a brightness property with a range check

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
@@ -41,6 +41,13 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
 
 class SmartLightDevice(deviceName: String, deviceCategory: String) :
     SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
 }
 
 fun main() {
