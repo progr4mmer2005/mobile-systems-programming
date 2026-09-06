0f422b0e99cecdf55b805176b3328c727b14f35e
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:02:25 +0300

    feat(conditionals): match prime numbers with one branch per value

---
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/03-prime-number.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val x = 3
+
+    when (x) {
+        2 -> println("x is a prime number between 1 and 10.")
+        3 -> println("x is a prime number between 1 and 10.")
+        5 -> println("x is a prime number between 1 and 10.")
+        7 -> println("x is a prime number between 1 and 10.")
+    }
+}
