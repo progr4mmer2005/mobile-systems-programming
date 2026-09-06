395a672d0ac2525abc1be5dd7b7b589c873c1d32
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:07:12 +0300

    feat(conditionals): return values from the branches instead of printing

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/04-traffic-light-if-expression.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/04-traffic-light-if-expression.kt
@@ -1,13 +1,9 @@
 fun main() {
     val trafficLightColor = "Black"
 
-    val message = if (trafficLightColor == "Red") {
-        println("Stop")
-    } else if (trafficLightColor == "Yellow") {
-        println("Slow")
-    } else if (trafficLightColor == "Green") {
-        println("Go")
-    } else {
-        println("Invalid traffic-light color")
-    }
+    val message =
+        if (trafficLightColor == "Red") "Stop"
+        else if (trafficLightColor == "Yellow") "Slow"
+        else if (trafficLightColor == "Green") "Go"
+        else "Invalid traffic-light color"
 }
