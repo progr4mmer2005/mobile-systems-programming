5b78a10172079dd883e828aa39c5a9843ca11d28
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:06:56 +0300

    feat(conditionals): start from the if/else statement to convert it

---
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/04-traffic-light-if-expression.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+}
