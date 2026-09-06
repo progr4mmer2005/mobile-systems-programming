ceebc96799187bc8d08f666643d9065a76c7f28f
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:01:14 +0300

    feat(conditionals): remove the if/else chain before rewriting it with when

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/02-traffic-light.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/02-traffic-light.kt
@@ -1,13 +1,3 @@
 fun main() {
     val trafficLightColor = "Black"
-
-    if (trafficLightColor == "Red") {
-        println("Stop")
-    } else if (trafficLightColor == "Yellow") {
-        println("Slow")
-    } else if (trafficLightColor == "Green") {
-        println("Go")
-    } else {
-        println("Invalid traffic-light color")
-    }
 }
