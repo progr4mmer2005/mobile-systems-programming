8d097ad0b96454b8b9c758f9f233dff73c4c1ad8
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 18:14:27 +0300

    refactor(function-types): pass null instead of an unused cupcake lambda

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/04-function-types-and-lambdas/01-trick-function.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/04-function-types-and-lambdas/01-trick-function.kt
@@ -3,12 +3,8 @@ fun main() {
         "$quantity quarters"
     }
 
-    val cupcake: (Int) -> String = {
-        "Have a cupcake!"
-    }
-
     val treatFunction = trickOrTreat(false, coins)
-    val trickFunction = trickOrTreat(true, cupcake)
+    val trickFunction = trickOrTreat(true, null)
     treatFunction()
     trickFunction()
 }
