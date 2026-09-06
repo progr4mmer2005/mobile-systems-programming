d369d1c606231e5e97055c553858367ecb4f9cd9
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 18:15:26 +0300

    refactor(function-types): move the lambda out of the parentheses

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/04-function-types-and-lambdas/01-trick-function.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/04-function-types-and-lambdas/01-trick-function.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val treatFunction = trickOrTreat(false, { "$it quarters" })
+    val treatFunction = trickOrTreat(false) { "$it quarters" }
     val trickFunction = trickOrTreat(true, null)
     treatFunction()
     trickFunction()
