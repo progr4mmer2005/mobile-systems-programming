93b851892b5353ef3d65c0cbd5c6e1abaad9a70f
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 13:20:33 +0300

    feat(lab-01): end the greeting message with an exclamation mark

--- lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     GreetingText(
-                        message = "Happy Birthday Denis",
+                        message = "Happy Birthday Denis!",
                         from = "From Denis",
                         modifier = Modifier
                             .padding(innerPadding)
@@ -65,6 +65,6 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Denis", from = "From Denis")
+        GreetingText(message = "Happy Birthday Denis!", from = "From Denis")
     }
 }
