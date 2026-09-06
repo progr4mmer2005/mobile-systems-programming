f40287b4b97f767e3d8a55a77588eff7f980ed56
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-07 00:06:46 +0300

    feat(dice-roller): create a vertical layout

--- lab-02/unit-2-building-app-ui/pathway-2-add-a-button-to-an-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ lab-02/unit-2-building-app-ui/pathway-2-add-a-button-to-an-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
@@ -36,4 +37,10 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        
+    }
 }
