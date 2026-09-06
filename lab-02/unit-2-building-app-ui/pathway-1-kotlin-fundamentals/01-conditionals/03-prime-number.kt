b241087fdc111add398dbb262142562b05ddfc08
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:02:54 +0300

    feat(conditionals): match a range of values with the in keyword

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/03-prime-number.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/03-prime-number.kt
@@ -3,6 +3,7 @@ fun main() {
 
     when (x) {
         2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
         else -> println("x isn't a prime number between 1 and 10.")
     }
 }
