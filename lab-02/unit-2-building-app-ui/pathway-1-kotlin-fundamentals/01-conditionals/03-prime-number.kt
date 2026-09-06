ac89f44fb6adaccf3803ea2eb3159381dd78c0ee
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:02:32 +0300

    feat(conditionals): add an else branch for non-prime values

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/03-prime-number.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/01-conditionals/03-prime-number.kt
@@ -6,5 +6,6 @@ fun main() {
         3 -> println("x is a prime number between 1 and 10.")
         5 -> println("x is a prime number between 1 and 10.")
         7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
     }
 }
