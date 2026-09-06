206707d118026ab988643915b9b3f1eea0307c1b
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 13:17:03 +0300

    feat(lab-01): temporarily shrink the message font to see the row layout

--- lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -33,7 +33,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Row {
         Text(
             text = message,
-            fontSize = 100.sp,
+            fontSize = 30.sp,
             lineHeight = 116.sp,
         )
         Text(
