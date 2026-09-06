a32da250eb4adf2e731ebb7ba00bde2851347755
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:57:31 +0300

    refactor(classes-and-objects): call the superclass implementation with super

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
@@ -39,7 +39,7 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
     }
 
     override fun turnOn() {
-        deviceStatus = "on"
+        super.turnOn()
         println(
             "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
                 "set to $channelNumber."
@@ -47,7 +47,7 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
     }
 
     override fun turnOff() {
-        deviceStatus = "off"
+        super.turnOff()
         println("$name turned off")
     }
 }
@@ -68,13 +68,13 @@ class SmartLightDevice(deviceName: String, deviceCategory: String) :
     }
 
     override fun turnOn() {
-        deviceStatus = "on"
+        super.turnOn()
         brightnessLevel = 2
         println("$name turned on. The brightness level is $brightnessLevel.")
     }
 
     override fun turnOff() {
-        deviceStatus = "off"
+        super.turnOff()
         brightnessLevel = 0
         println("Smart Light turned off")
     }
