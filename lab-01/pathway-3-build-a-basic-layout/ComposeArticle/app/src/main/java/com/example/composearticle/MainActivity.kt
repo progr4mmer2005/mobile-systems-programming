4d230fc7337b9fd0e501dc79acaeb2aa0f639cd7
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 14:59:38 +0300

    feat(compose-article): add the article title

--- lab-01/pathway-3-build-a-basic-layout/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -10,10 +10,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,6 +41,11 @@ fun ComposeArticleApp(modifier: Modifier = Modifier) {
             painter = painterResource(R.drawable.bg_compose_background),
             contentDescription = null,
             modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = stringResource(R.string.title_jetpack_compose_tutorial),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
         )
     }
 }
