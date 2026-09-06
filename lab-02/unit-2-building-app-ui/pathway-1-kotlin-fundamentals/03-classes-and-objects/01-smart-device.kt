3034c7713586be2d167b208ecc5c5914df2b155c
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 17:52:15 +0300

    feat(classes-and-objects): add a SmartLightDevice subclass

--- lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
+++ lab-02/unit-2-building-app-ui/pathway-1-kotlin-fundamentals/03-classes-and-objects/01-smart-device.kt
@@ -39,6 +39,10 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
     }
 }
 
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+}
+
 fun main() {
     val smartTvDevice = SmartDevice(name = "Android TV", category = "Entertainment")
     println("Device name is: ${smartTvDevice.name}")
