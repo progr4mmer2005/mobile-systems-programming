7306692a30f6b0e0441dcaa5141e3ec9340e5267
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 14:45:38 +0300

    feat(lab-01): pass the modifier through to the Box

--- lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -67,7 +67,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
-    Box {
+    Box(modifier) {
         Image(
             painter = image,
             contentDescription = null
