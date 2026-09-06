71c29167a370ff1bddb0271717f0a14344d402bc
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 13:17:03 +0300

    feat(lab-01): restore the message font size to 100.sp

--- lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -33,7 +33,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Row {
         Text(
             text = message,
-            fontSize = 30.sp,
+            fontSize = 100.sp,
             lineHeight = 116.sp,
         )
         Text(
