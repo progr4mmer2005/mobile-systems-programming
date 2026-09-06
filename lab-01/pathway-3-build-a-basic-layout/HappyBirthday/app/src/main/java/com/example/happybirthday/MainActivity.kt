a97eb5c2470f0e613fa122a517e801e0d46e5e7a
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-06 13:20:16 +0300

    feat(lab-01): center the column contents vertically

--- lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ lab-01/pathway-3-build-a-basic-layout/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -35,7 +36,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
         Text(
             text = message,
             fontSize = 100.sp,
