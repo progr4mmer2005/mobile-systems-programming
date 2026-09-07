e5863138e8eefa6ea1a424607c092dee1d846d89
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-07 13:57:52 +0300

    feat(tip-time): keep the state with remember

--- lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/01-intro-to-state-in-compose/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/01-intro-to-state-in-compose/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -33,7 +33,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -89,11 +92,11 @@ fun TipTimeLayout() {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    var amountInput = mutableStateOf("0")
+    var amountInput by remember { mutableStateOf("") }
 
     TextField(
-        value = amountInput.value,
-        onValueChange = { amountInput.value = it },
+        value = amountInput,
+        onValueChange = { amountInput = it },
         modifier = modifier
     )
 }
