5b9e08d01a415590d9fb4526bd9374a409954f61
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-07 14:02:27 +0300

    feat(tip-time): parse the input into a number

--- lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/01-intro-to-state-in-compose/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/01-intro-to-state-in-compose/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -95,6 +95,7 @@ fun TipTimeLayout() {
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
     var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
 
     TextField(
         value = amountInput,
