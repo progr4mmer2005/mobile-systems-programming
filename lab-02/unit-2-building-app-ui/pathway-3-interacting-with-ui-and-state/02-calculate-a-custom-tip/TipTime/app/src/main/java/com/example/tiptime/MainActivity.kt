8604243772404e67c236eccc4157d4684b43ba37
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-08 16:46:47 +0300

    feat(custom-tip): add a next action button to the keyboard

--- lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/02-calculate-a-custom-tip/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/02-calculate-a-custom-tip/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -42,6 +42,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -122,7 +123,10 @@ fun EditNumberField(
         onValueChange = onValueChanged,
         singleLine = true,
         label = { Text(stringResource(label)) },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next
+        ),
         modifier = modifier
     )
 }
