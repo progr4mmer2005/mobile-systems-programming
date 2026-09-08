fba3d28aa1e857d0ddd45a3922519d4bc12a3c75
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-08 17:00:01 +0300

    feat(custom-tip): round the tip up when the switch is on

--- lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/02-calculate-a-custom-tip/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/02-calculate-a-custom-tip/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -175,8 +175,11 @@ fun RoundTheTipRow(
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     val tip = tipPercent / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
