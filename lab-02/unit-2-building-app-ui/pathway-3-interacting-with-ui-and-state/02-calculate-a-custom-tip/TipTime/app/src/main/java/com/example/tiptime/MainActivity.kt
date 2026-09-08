5edaaff9e35cc6627d7b9fe0e13af3bea1985080
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-08 16:59:53 +0300

    feat(custom-tip): show the switch and keep its state

--- lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/02-calculate-a-custom-tip/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/02-calculate-a-custom-tip/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -73,6 +73,7 @@ class MainActivity : ComponentActivity() {
 fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
     var tipInput by remember { mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false) }
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
     val tip = calculateTip(amount, tipPercent)
@@ -114,6 +115,11 @@ fun TipTimeLayout() {
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
+        )
+        RoundTheTipRow(
+            roundUp = roundUp,
+            onRoundUpChanged = { roundUp = it },
+            modifier = Modifier.padding(bottom = 32.dp)
         )
         Text(
             text = stringResource(R.string.tip_amount, tip),
