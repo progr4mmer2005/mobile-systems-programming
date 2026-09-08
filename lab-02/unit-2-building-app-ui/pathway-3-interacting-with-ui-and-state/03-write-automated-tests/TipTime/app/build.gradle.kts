9ad767a89e96ee332c3ad350bab86f1e6d0e1d32
progr4mmer2005 <dennaumol05@gmail.com>
2026-09-08 17:57:06 +0300

    fix(tip-time-tests): update espresso so ui tests run on android 17

--- lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/03-write-automated-tests/TipTime/app/build.gradle.kts
+++ lab-02/unit-2-building-app-ui/pathway-3-interacting-with-ui-and-state/03-write-automated-tests/TipTime/app/build.gradle.kts
@@ -78,8 +78,8 @@ dependencies {
 
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
